260619c99b3a8b537aee4f282985cd15a710ec9e
Phil Hindman <1756135+pahindman@users.noreply.github.com>
2025-04-25 15:35:23 -0500

    Update server.go

--- internal/server.go
+++ internal/server.go
@@ -12,8 +12,8 @@ import (
 	"text/template"
 	"time"
 
-	"github.com/bernardo-bruning/ollama-copilot/internal/handlers"
-	"github.com/bernardo-bruning/ollama-copilot/internal/middleware"
+	"github.com/pahindman/ollama-copilot/internal/handlers"
+	"github.com/pahindman/ollama-copilot/internal/middleware"
 	"github.com/ollama/ollama/api"
 )
 
